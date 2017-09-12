c4600e8562b85703e64eda4e8c24c7618ff3ec3e
mkiterian <hmukite@gmail.com>
2017-09-12 23:10:02 +0300

    [Feature] Modify miu theme to use a blue primary color

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import ParentComponet from './parentComponet.jsx';
+import { blueGrey800, blueA700 } from 'material-ui/styles/colors';
 import '../css/style.scss';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: blueGrey800,
+    primary1Color: blueA700,
+  },
+  appBar: {
+    height: 50,
+  },
+});
 
 export default class App extends React.Component {
   render() {
     const style = {
-      height: 100,
       font: 'Georgia',
     };
     return (
       <div>
-        <MuiThemeProvider>
+        <MuiThemeProvider muiTheme={muiTheme}>
           <AppBar
-            title="FellaDoor"
+            title="Felladoor"
+            titleStyle={{ marginLeft: 52 + '%' }}
             showMenuIconButton={false}
             style={style}
           />
